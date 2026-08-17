4d89ebb1c9f172d774678833774b3d4c985b5b88
DOrtizSalcedo <david.osalcedo@mail.escuelaing.edu.co>
2026-08-17 17:01:48 -0500

    feature: added challenge 2 code

--- src/main/java/edu/eci/dosw/Application.java
+++ src/main/java/edu/eci/dosw/Application.java
@@ -1,10 +1,12 @@
 package edu.eci.dosw;
 
 import edu.eci.dosw.challenge1.Challenge1PepeStore;
+import edu.eci.dosw.challenge2.Challenge2FiveStarChef;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("Maven project configured and running correctly.");
-        Challenge1PepeStore.run();
+        //Challenge1PepeStore.run();
+        Challenge2FiveStarChef.run();
     }
 }
